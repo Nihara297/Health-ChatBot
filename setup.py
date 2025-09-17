1fb198cb893484ff586cabcc63731ae48398a2d7
Nihara <nihara.rajesh2023@vitstudent.ac.in>
2025-09-17 11:55:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Nihara Rajesh",
+    author_email="nihara.rajesh2023@vitstudent.ac.in",
+    packages=find_packages(),
+    install_requires=[]
+)
